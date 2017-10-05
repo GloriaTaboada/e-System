9f8aeba9c8f022f33e1b6822bdb07a9e23d8e9a9
Oli <Oli@Oli-VAIO>
2017-10-05 20:20:07 +0200

    Commit inicial

---
+++ ClassClient.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class ClassClient {
+    
+    var $nombre; 
+    var $numero; 
+
+    function __construct($nombre,$numero){ 
+        $this->nombre=$nombre; 
+        $this->numero=$numero;
+    } 
+    
+
+}
